3163c297f8c633355a99ec68a62c9dee8dba48b1
Joedmo15 <58199979+Joedmo15@users.noreply.github.com>
2020-06-04 21:26:05 -0600

    Working on Neural Network

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,3 +1,33 @@
 import random
+import numpy
+
+
+def sigmoid_function(number):
+    return 1/(1+2.71828**-number)
+
+
+def convert(array):
+    size = array.size
+    x = 0
+    while x < size:
+        array[x][0] = sigmoid_function(array[x][0])
+        x += 1
+    return array
+
+
 class NeuralNetwork():
-    def __init__(self):
+    def __init__(self, inputs, outputs):
+        self.hidden = round(inputs * 0.6666, 0)
+        self.inputs = inputs
+        self.outputs = outputs
+        self.input_hidden_weights = numpy.rand((inputs.size, 1))
+        self.hidden_output_weights = numpy.rand((self.hidden, 1))
+
+    def FeedForward(self):
+        self.hidden_layer = convert(numpy.dot(self.inputs, self.input_hidden_weights))
+        self.output_layer = convert(numpy.dot(self.hidden_layer, self.hidden_output_weights))
+
+
+
+
+
